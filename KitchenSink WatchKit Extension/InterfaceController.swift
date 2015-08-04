d1a3ea7c9a16c672dad1d2c71c727654458adfd2
Christopher Stott <christopherstott@gmail.com>
2015-08-04 11:52:17 -0700

    Adding watch app

---
+++ KitchenSink WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  KitchenSink WatchKit Extension
+//
+//  Created by Chris on 2015-08-04.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}

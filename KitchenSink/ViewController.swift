695e253bc64f360c3a8ae77948d79833e13907d3
Christopher Stott <christopherstott@gmail.com>
2015-08-04 11:49:47 -0700

    Initial Commit

---
+++ KitchenSink/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KitchenSink
+//
+//  Created by Chris on 2015-08-04.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

4457ac012429843cb596bbf8373324f08e1ac793
Christopher Stott <christopherstott@gmail.com>
2015-08-06 11:15:39 -0700

    Adding KitchenKit framework

---
+++ KitchenKit/KitchenKit.h
@@ -0,0 +1,19 @@
+//
+//  KitchenKit.h
+//  KitchenKit
+//
+//  Created by Chris on 2015-08-06.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KitchenKit.
+FOUNDATION_EXPORT double KitchenKitVersionNumber;
+
+//! Project version string for KitchenKit.
+FOUNDATION_EXPORT const unsigned char KitchenKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KitchenKit/PublicHeader.h>
+
+
